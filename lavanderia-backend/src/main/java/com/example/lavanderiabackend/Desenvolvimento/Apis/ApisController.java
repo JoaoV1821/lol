10732c5384a789c55552a5de2e166e0ccd4361f8
Samuel <samueldzsantos@gmail.com>
2023-08-27 14:52:03 -0300

    Mudanças na tela de consulta

--- lavanderia-backend/src/main/java/com/example/lavanderiabackend/Desenvolvimento/Apis/ApisController.java
+++ lavanderia-backend/src/main/java/com/example/lavanderiabackend/Desenvolvimento/Apis/ApisController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class ApisController {
 
     @Autowired
--- lavanderia-backend/src/main/java/com/example/lavanderiabackend/LavanderiaBackendApplication.java
+++ lavanderia-backend/src/main/java/com/example/lavanderiabackend/LavanderiaBackendApplication.java
@@ -2,8 +2,13 @@ package com.example.lavanderiabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
+@CrossOrigin(originPatterns = "*")
 public class LavanderiaBackendApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +16,8 @@ public class LavanderiaBackendApplication {
 
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
