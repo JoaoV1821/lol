5ffa48a7b3f6091bb10ff2b8dc9f248b287398a2
yuki <yuki.alexandre@ufpr.br>
2023-09-10 22:39:32 -0300

    Adicao do modulo de funcionario

--- lavanderia-online/src/app/app-routing.module.ts
+++ lavanderia-online/src/app/app-routing.module.ts
@@ -9,52 +9,44 @@ import { PagamentoComponent } from './pagamento';
 import { PaginaInicialComponent } from './pagina-inicial';
 import { ListarPedidosAbertosComponent } from './pagina-inicial-funcionario';
 import { PedidoComponent } from './pedido';
+import { ListarRoupasComponent } from './crud-roupas/listar-roupas';
+import { InserirRoupaComponent } from './crud-roupas/inserir-roupa';
+import { EditarRoupaComponent } from './crud-roupas/editar-roupa';
+import { ListarFuncionarioComponent } from './funcionario';
+import { InserirFuncionarioComponent } from './funcionario';
+import { EditarFuncionarioComponent } from './funcionario';
 
 
 const routes: Routes = [
-  { path: '',
-    redirectTo: '',
-    pathMatch: 'full'},
+  { path: '', redirectTo: '', pathMatch: 'full'},
     
-  { path: 'autocadastro',
-    redirectTo: 'autocadastro/autocadastro'},
-  { path: 'autocadastro/autocadastro',
-    component: AutocadastroComponent },
+  { path: 'autocadastro', component: AutocadastroComponent },
 
-  { path: 'consulta-pedido',
-    redirectTo: 'consulta-pedido/consulta-pedido'},
-  { path: 'consulta-pedido/consulta-pedido',
-    component: ConsultaPedidoComponent },
+  { path: 'consulta-pedido', component: ConsultaPedidoComponent },
 
-  { path: 'login',
-    redirectTo: 'login/login'},
-  { path: 'login/login',
-    component: LoginComponent },
+  { path: 'login', component: LoginComponent },
 
-  { path: 'orcamento',
-    redirectTo: 'orcamento/orcamento'},
-  { path: 'orcamento/orcamento',
-    component: OrcamentoComponent },
+  { path: 'orcamento', component: OrcamentoComponent },
 
-  { path: 'pagamento',
-    redirectTo: 'pagamento/pagamento'},
-  { path: 'pagamento/pagamento',
-    component: PagamentoComponent },
+  { path: 'pagamento', component: PagamentoComponent },
 
-  { path: 'pagina-inicial',
-    redirectTo: 'pagina-inicial/pagina-inicial'},
-  { path: 'pagina-inicial/pagina-inicial',
-    component: PaginaInicialComponent },
+  { path: 'pagina-inicial', component: PaginaInicialComponent },
 
-  { path: 'pagina-inicial-funcionario',
-    redirectTo: 'pagina-inicial-funcionario/listar-pedidos-aberto'},
-  { path: 'pagina-inicial-funcionario/listar-pedidos-aberto',
-    component: ListarPedidosAbertosComponent },
+  { path: 'pagina-inicial-funcionario', component: ListarPedidosAbertosComponent },
 
-  { path: 'pedido',
-    redirectTo: 'pedido/pedido'},
-  { path: 'pedido/pedido',
-    component: PedidoComponent }
+  { path: 'pedido', component: PedidoComponent },
+  
+  //CRUD Roupas
+  { path: 'roupas', redirectTo: 'roupas/listar-roupas'},
+  { path: 'roupas/listar-roupas', component: ListarRoupasComponent},
+  { path: 'roupas/novo', component: InserirRoupaComponent},
+  { path: 'roupas/editar/:id', component: EditarRoupaComponent},
+
+  //CRUD Funcionarios
+  { path: 'funcionarios', redirectTo: 'funcionarios/listar'},
+  { path: 'funcionarios/listar', component: ListarFuncionarioComponent },
+  { path: 'funcionarios/novo', component: InserirFuncionarioComponent },
+  { path: 'funcionarios/editar/:cpf', component: EditarFuncionarioComponent},
 
 ];
 
