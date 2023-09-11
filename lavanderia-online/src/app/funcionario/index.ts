d560b1c7e7eb5cef74be67022c2a8939dedfedba
yuki <yuki.alexandre@ufpr.br>
2023-09-10 22:26:41 -0300

    Criação do modulo de Funcionario (CRUD)

---
+++ lavanderia-online/src/app/funcionario/index.ts
@@ -0,0 +1,5 @@
+export * from './funcionario.module';
+export * from './listar-funcionario';
+export * from './inserir-funcionario';
+export * from './editar-funcionario';
+export * from './services';
