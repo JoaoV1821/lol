3fe7c752d725acae998f6c24b05413b4672d8557
Samuel <samueldzsantos@gmail.com>
2023-10-09 14:34:07 -0300

    Ajustes Carrinho , Cadastro e outros

--- lavanderia-backend/src/main/java/com/example/lavanderiabackend/models/Cadastro/Cadastro.java
+++ lavanderia-backend/src/main/java/com/example/lavanderiabackend/models/Cadastro/Cadastro.java
@@ -1,5 +1,12 @@
 package com.example.lavanderiabackend.models.Cadastro;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.example.lavanderiabackend.models.Cadastro.DTO.CadastroModelo;
 import com.example.lavanderiabackend.models.Endereco.Endereco;
 
@@ -11,17 +18,20 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"nome","sobrenome"}))
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Cadastro {
+public class Cadastro implements UserDetails {
     @Id
     @SequenceGenerator(name = "cadastro_sequence", sequenceName = "cadastro_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cadastro_sequence")
@@ -38,9 +48,12 @@ public class Cadastro {
     private String senha;
     @Column(nullable = false)
     private String telefone;
+    @Column(nullable=false)
+    private Papeis papel;
     @ManyToOne
     @JoinColumn(name = "endereco_id", nullable = false)
     private Endereco endereco;
+    
 
     public Cadastro(CadastroModelo modelo) {
         this.cpf = modelo.getCpf();
@@ -48,5 +61,49 @@ public class Cadastro {
         this.telefone = modelo.getTelefone();
         this.nome = modelo.getNome();
         this.sobrenome = modelo.getSobrenome();
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.papel == Papeis.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        
+    }
+
+
+    @Override
+    public String getPassword() {
+        return this.getSenha();
+    }
+
+
+    @Override
+    public String getUsername() {
+        return this.getNome() + " " + this.getSobrenome();
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
